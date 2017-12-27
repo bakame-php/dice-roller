8578559f7b661267029d96773b0e08cd79e67806
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2017-12-27 18:00:03 +0100

    update PHP file header

--- src/Rollable.php
+++ src/Rollable.php
@@ -1,8 +1,15 @@
 <?php
 /**
- * It's a dice-cup: you put your die in the cup, you shake it and then you get the result.
- * @author Bertrand Andres
- */
+* This file is part of the League.csv library
+*
+* @license http://opensource.org/licenses/MIT
+* @link https://github.com/bakame-php/dice-roller/
+* @version 1.0.0
+* @package bakame-php/dice-roller
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 namespace Bakame\DiceRoller;
 
